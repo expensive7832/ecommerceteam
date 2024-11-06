dd98851a1f15e83b5266efbcf046e0dd177be7f9
YOUNG_PRINCE <adesemowomuhammed55663@gmail.com>
2024-11-06 11:24:30 +0100

    done

--- script.js
+++ script.js
@@ -6,6 +6,34 @@ let count = 0;
         }
 
         function decrement() {
-            count--;
+            if (count <= 0) {
+                count = 0;
+            }
+            else{
+                count--;
+            }
             document.getElementById("number").textContent = count;
         }
+
+
+let products = [
+    { name: "mirror", description: "ytyuruhryurhguryfiuufuruuurui", price: 1500 , img:""},
+    { name: "phone", description: "ytyuruhryurhguryfiuufuruuurui", price: 1500 },
+    { name: "key bord", description: "ytyuruhryurhguryfiuufuruuurui", price: 1500 },
+    { name: "mirror", description: "ytyuruhryurhguryfiuufuruuurui", price: 1500 },
+    { name: "mirror", description: "ytyuruhryurhguryfiuufuruuurui", price: 1500 },
+]
+
+products.forEach((product) => {
+    const prod = document.getElementsByClassName('pop');
+    prod.className = 'produc';
+    prod = `<div class="java d-flex mt-3">
+                    <img class="d-inline" src="./img/03.png" alt="pro">
+                    <div class="">
+                        <p class="pp">Bose Sport Earbuds - Wireless Earphones - Bluetooth In Ear...</p>
+                        <p >${product.price}</p>
+                    </div>     
+                </div>`
+    product.appendChild(prod);
+});
+  
